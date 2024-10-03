1b53807ec50da00b00bd85e97d938442b946f6d8
ParvathyReghunath <parvathyreghunath.mec@gmail.com>
2024-10-03 12:05:46 +0530

    Created login and logout

--- ecommercewebsite/store/urls.py
+++ ecommercewebsite/store/urls.py
@@ -3,5 +3,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('login/',views.login_user,name='login'),
+    path('logout/',views.logout_user,name='logout'),
 ]
--- ecommercewebsite/store/views.py
+++ ecommercewebsite/store/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
 from .models import Product
-
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
 def home(request):
     products = Product.objects.all()
     return render(request,'home.html',{'products':products})
+
+
+def about(request):
+    return render(request,'about.html')
+
+def login_user(request):
+    if  request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,("You have been successfully logged in"))
+            return redirect('home')
+        else:
+            messages.success(request,("There was an error"))
+            return render(request,'login.html')
+    else:
+        return render(request,'login.html',{})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,("You have been logged out"))
+    return redirect('home')
+
+
