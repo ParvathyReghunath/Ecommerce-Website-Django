dd06c4a758285f95eef4899e9fdd04fb4389f2a4
ParvathyReghunath <parvathyreghunath.mec@gmail.com>
2024-10-18 15:52:13 +0530

    ADD to cart modified (not implemented)

--- ecommercewebsite/cart/cart.py
+++ ecommercewebsite/cart/cart.py
@@ -14,3 +14,12 @@ class Cart():
 
         # to access it in all pages
         self.cart=cart
+
+
+    def add(self,product):
+        product_id=str(product.id)
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id]={'price':str(product.price)}
+        self.session.modified= True
--- ecommercewebsite/cart/views.py
+++ ecommercewebsite/cart/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 def cart_summary(request):
     return render(request,'cart_summary.html')
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+    
+    if request.POST.get('action')=='POST':
+        product_id = request.POST.get('product_id')
+        product=get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
 
+        response=JsonResponse({'Product Name' : product.name})
+        return response
+    
 def cart_update(request):
     pass
 
